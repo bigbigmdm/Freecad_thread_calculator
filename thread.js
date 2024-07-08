33eba10dd1447476dd8adfded2341888637c7a8a
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2024-07-08 14:30:47 +0300

    Hole diameter added

--- thread.js
+++ thread.js
@@ -25,7 +25,8 @@ let inchstep =  [ 0.907, 1.337, 1.337, 1.814, 1.814, 1.814, 1.814, 2.309, 2.309,
 <circle cx="10" cy="100" r="6" class="pf" />
 <circle cx="210" cy="158" r="6" class="pf" />
 <path d="M4,76l12 -12m-12 80l12 -12m36 17l8 -14m0 10l8 -14m-14 -66l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,160v24m200 0 v-24v18h-200m-0-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<path d="M10,160v48v-6h220h-220v-18m200 0 v-24v18h-200m-0-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<use href="#arrow" x="28" y="208" transform="rotate(-90 28 208)"/>
 <use href="#arrow" x="28" y="184" transform="rotate(-90 28 184)"/>
 <use href="#arrow" x="190" y="172" transform="rotate(90 190 172)"/>
 <use href="#arrow" x="28" y="28" transform="rotate(-90 28 28)"/>
@@ -41,8 +42,10 @@ firstsvg1 = `
 <circle cx="110" cy="100" r="6" class="mf" />
 <circle cx="210" cy="152" r="6" class="mf" />
 <path d="M104,80l12 -12m-12 68l12 -12m-64 -39l8 -14m0 10l8 -14m-22 63l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M110,152v24m100 0 v-24v18h-100m-100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
+<path d="M110,152v48v-6h120h-120v-18
+m100 0 v-24v18h-100m-100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
 <use href="#arrow" x="128" y="176" transform="rotate(-90 128 176)"/>
+<use href="#arrow" x="128" y="200" transform="rotate(-90 128 200)"/>
 <use href="#arrow" x="190" y="164" transform="rotate(90 190 164)"/>
 <use href="#arrow" x="28" y="38" transform="rotate(-90 28 38)"/>
 <use href="#arrow" x="90" y="26" transform="rotate(90 90 26)"/>
@@ -56,9 +59,10 @@ firstsvg2 = `
 <circle cx="100" cy="100" r="6" class="pf" />
 <circle cx="10" cy="158" r="6" class="pf" />
 <path d="M94,76l12 -12m-12 80l12 -12m36 17l8 -14m0 10l8 -14m-14 -66l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,160v24m90 0 v-24v18h-90m90-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<path d="M10,160v24m90 0 v-24v48v-8h-100h100v-40v18h-90m90-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
 <use href="#arrow" x="28" y="184" transform="rotate(-90 28 184)"/>
 <use href="#arrow" x="80" y="172" transform="rotate(90 80 172)"/>
+<use href="#arrow" x="80" y="194" transform="rotate(90 80 194)"/>
 <use href="#arrow" x="118" y="28" transform="rotate(-90 118 28)"/>
 <use href="#arrow" x="180" y="16" transform="rotate(90 180 16)"/>
 <use href="#arrow" x="226" y="60" transform="rotate(180 226 60)"/>
@@ -73,9 +77,11 @@ firstsvg3 = `
 <circle cx="210" cy="102" r="6" class="mf" />
 <circle cx="10" cy="152" r="6" class="mf" />
 <path d="M204,80l12 -12m-12 68l12 -12m-64 -39l8 -14m0 10l8 -14m-22 63l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,152v24m200 0 v-24v18h-200m100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
+<path d="M10,152v24m200 0 v-24v48v-8h-220h220v-40
+v18h-200m100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
 <use href="#arrow" x="28" y="176" transform="rotate(-90 28 176)"/>
 <use href="#arrow" x="190" y="164" transform="rotate(90 190 164)"/>
+<use href="#arrow" x="190" y="186" transform="rotate(90 190 186)"/>
 <use href="#arrow" x="128" y="38" transform="rotate(-90 128 38)"/>
 <use href="#arrow" x="190" y="26" transform="rotate(90 190 26)"/>
 <use href="#arrow" x="187" y="85" transform="rotate(-40 187 85)"/>
@@ -89,17 +95,21 @@ firstsvg3 = `
         step = Number((document.getElementById("step").value).replace(",","."));
         
         middlesvg = '<text x="80" y="174" font-size="14px" >' + (size/2 + 0.1082 * step).toFixed(2) + ' mm</text>';
+        middlesvg = middlesvg + '<text x="80" y="196" font-size="14px" > ⌀' + (size + 0.2164 * step).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text font-size="14px" transform="translate(150, 130) rotate(-90)">' + (0.25*step).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text x="30" y="14" font-size="14px" >' + (0.6494*step - 0.02).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text font-size="14px" transform="translate(46, 110) rotate(-90)">60°</text>';        
-        middlesvg1 = '<text x="130" y="162" font-size="14px" >' + (size/2 - 0.5412 * step).toFixed(2) + ' mm</text>';        
+        middlesvg1 = '<text x="130" y="162" font-size="14px" >' + (size/2 - 0.5412 * step).toFixed(2) + ' mm</text>';
+        middlesvg1 = middlesvg1 + '<text x="130" y="188" font-size="14px" > ⌀' + (size - 1.0824 * step).toFixed(2) + ' mm</text>';
         middlesvg1 = middlesvg1 + '<text x="30" y="20" font-size="14px" >' + (0.6494 * step).toFixed(2) + ' mm</text>';
         middlesvg1 = middlesvg1 + '<text font-size="14px" transform="translate(76, 90) rotate(90)">60°</text>';       
         middlesvg2 = '<text x="20" y="170" font-size="14px" >' + (size/2 - 0.7577*step).toFixed(2) + ' mm</text>';
+        middlesvg2 = middlesvg2 + '<text x="10" y="194" font-size="14px" > ⌀' + (size - 1.5154*step).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text font-size="14px" transform="translate(240, 130) rotate(-90)">' + (0.125*step).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text x="120" y="14" font-size="14px" >' + (0.7577*step - 0.02).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text font-size="14px" transform="translate(136, 110) rotate(-90)">60°</text>';  
-        middlesvg3 = '<text x="80" y="162" font-size="14px" >' + (size/2).toFixed(2) + ' mm</text>';        
+        middlesvg3 = '<text x="80" y="162" font-size="14px" >' + (size/2).toFixed(2) + ' mm</text>';
+        middlesvg3 = middlesvg3 + '<text x="80" y="186" font-size="14px" > ⌀' + (size).toFixed(2) + ' mm</text>';
         middlesvg3 = middlesvg3 + '<text x="130" y="20" font-size="14px" >' + (0.7577 * step).toFixed(2) + ' mm</text>';
         middlesvg3 = middlesvg3 + '<text font-size="14px" transform="translate(176, 90) rotate(90)">60°</text>';        
         document.getElementById("img1").innerHTML = firstsvg + middlesvg + lastsvg
@@ -230,7 +240,8 @@ firstsvg3 = `
 <circle cx="10" cy="100" r="6" class="pf" />
 <circle cx="210" cy="158" r="6" class="pf" />
 <path d="M4,76l12 -12m-12 80l12 -12m36 17l8 -14m0 10l8 -14m-14 -66l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,160v24m200 0 v-24v18h-200m-0-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<path d="M10,160v48v-6h220h-220v-18m200 0 v-24v18h-200m-0-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<use href="#arrow" x="28" y="208" transform="rotate(-90 28 208)"/>
 <use href="#arrow" x="28" y="184" transform="rotate(-90 28 184)"/>
 <use href="#arrow" x="190" y="172" transform="rotate(90 190 172)"/>
 <use href="#arrow" x="28" y="28" transform="rotate(-90 28 28)"/>
@@ -246,8 +257,10 @@ firstsvg1 = `
 <circle cx="110" cy="100" r="6" class="mf" />
 <circle cx="210" cy="152" r="6" class="mf" />
 <path d="M104,80l12 -12m-12 68l12 -12m-64 -39l8 -14m0 10l8 -14m-22 63l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M110,152v24m100 0 v-24v18h-100m-100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
+<path d="M110,152v48v-6h120h-120v-18
+m100 0 v-24v18h-100m-100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
 <use href="#arrow" x="128" y="176" transform="rotate(-90 128 176)"/>
+<use href="#arrow" x="128" y="200" transform="rotate(-90 128 200)"/>
 <use href="#arrow" x="190" y="164" transform="rotate(90 190 164)"/>
 <use href="#arrow" x="28" y="38" transform="rotate(-90 28 38)"/>
 <use href="#arrow" x="90" y="26" transform="rotate(90 90 26)"/>
@@ -261,9 +274,10 @@ firstsvg2 = `
 <circle cx="100" cy="100" r="6" class="pf" />
 <circle cx="10" cy="158" r="6" class="pf" />
 <path d="M94,76l12 -12m-12 80l12 -12m36 17l8 -14m0 10l8 -14m-14 -66l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,160v24m90 0 v-24v18h-90m90-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
+<path d="M10,160v24m90 0 v-24v48v-8h-100h100v-40v18h-90m90-162v24m100 -24v63v-57h-100m100 58h24m0 40h-24h20v28v-94m-84 2q-38 40 0 87" class="dim"/>
 <use href="#arrow" x="28" y="184" transform="rotate(-90 28 184)"/>
 <use href="#arrow" x="80" y="172" transform="rotate(90 80 172)"/>
+<use href="#arrow" x="80" y="194" transform="rotate(90 80 194)"/>
 <use href="#arrow" x="118" y="28" transform="rotate(-90 118 28)"/>
 <use href="#arrow" x="180" y="16" transform="rotate(90 180 16)"/>
 <use href="#arrow" x="226" y="60" transform="rotate(180 226 60)"/>
@@ -278,9 +292,11 @@ firstsvg3 = `
 <circle cx="210" cy="102" r="6" class="mf" />
 <circle cx="10" cy="152" r="6" class="mf" />
 <path d="M204,80l12 -12m-12 68l12 -12m-64 -39l8 -14m0 10l8 -14m-22 63l16 -10m-8 12l16 -10" class="dim"/>
-<path d="M10,152v24m200 0 v-24v18h-200m100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
+<path d="M10,152v24m200 0 v-24v48v-8h-220h220v-40
+v18h-200m100-144v72m100 -50v-24v8h-100m70 35q34 34 0 70" class="dim"/>
 <use href="#arrow" x="28" y="176" transform="rotate(-90 28 176)"/>
 <use href="#arrow" x="190" y="164" transform="rotate(90 190 164)"/>
+<use href="#arrow" x="190" y="186" transform="rotate(90 190 186)"/>
 <use href="#arrow" x="128" y="38" transform="rotate(-90 128 38)"/>
 <use href="#arrow" x="190" y="26" transform="rotate(90 190 26)"/>
 <use href="#arrow" x="187" y="85" transform="rotate(-40 187 85)"/>
@@ -294,17 +310,21 @@ firstsvg3 = `
         istep = Number((document.getElementById("stepi").value).replace(",","."));
         ih = 1.920 * istep;
         middlesvg = '<text x="80" y="174" font-size="14px" >' + (isize/2 + ih / 12).toFixed(2) + ' mm</text>';
+        middlesvg = middlesvg + '<text x="80" y="198" font-size="14px" > ⌀' + (isize + ih / 6).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text font-size="14px" transform="translate(150, 130) rotate(-90)">' + (0.1539 * ih / 2).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text x="30" y="14" font-size="14px" >' + (3 * ih / 8).toFixed(2) + ' mm</text>';
         middlesvg = middlesvg + '<text font-size="14px" transform="translate(46, 110) rotate(-90)">55°</text>';        
-        middlesvg1 = '<text x="130" y="162" font-size="14px" >' + (isize/2 + ih / 12 - 3 * ih / 8).toFixed(2) + ' mm</text>';        
+        middlesvg1 = '<text x="130" y="162" font-size="14px" >' + (isize/2 + ih / 12 - 3 * ih / 8).toFixed(2) + ' mm</text>';
+        middlesvg1 = middlesvg1 + '<text x="130" y="188" font-size="14px" >⌀' + (isize + ih / 6 - 3 * ih / 4).toFixed(2) + ' mm</text>';
         middlesvg1 = middlesvg1 + '<text x="30" y="20" font-size="14px" >' + (0.425 * ih ).toFixed(2) + ' mm</text>';
         middlesvg1 = middlesvg1 + '<text font-size="14px" transform="translate(76, 90) rotate(90)">55°</text>';       
         middlesvg2 = '<text x="20" y="170" font-size="14px" >' + (isize/2 - 3*ih/8).toFixed(2) + ' mm</text>';
+        middlesvg2 = middlesvg2 + '<text x="10" y="194" font-size="14px" > ⌀' + (isize - 3*ih/4).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text font-size="14px" transform="translate(240, 130) rotate(-90)">' + (0.1539 * ih / 2).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text x="120" y="14" font-size="14px" >' + (3 * ih / 8).toFixed(2) + ' mm</text>';
         middlesvg2 = middlesvg2 + '<text font-size="14px" transform="translate(136, 110) rotate(-90)">55°</text>';  
-        middlesvg3 = '<text x="80" y="162" font-size="14px" >' + (isize/2).toFixed(2) + ' mm</text>';        
+        middlesvg3 = '<text x="80" y="162" font-size="14px" >' + (isize/2).toFixed(2) + ' mm</text>';
+        middlesvg3 = middlesvg3 + '<text x="80" y="186" font-size="14px" > ⌀' + (isize).toFixed(2) + ' mm</text>'; 
         middlesvg3 = middlesvg3 + '<text x="130" y="20" font-size="14px" >' + (0.425 * ih).toFixed(2) + ' mm</text>';
         middlesvg3 = middlesvg3 + '<text font-size="14px" transform="translate(176, 90) rotate(90)">55°</text>';        
         document.getElementById("img3").innerHTML = firstsvg + middlesvg + lastsvg
